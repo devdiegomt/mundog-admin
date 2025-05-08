8ba02f8356c1d2351b98549e4906155fffaf33d5
devdiegomt <devdiegomt@gmail.com>
2025-05-07 19:03:56 -0500

    feat: delete option.

--- src/components/products/ProductsCards.tsx
+++ src/components/products/ProductsCards.tsx
@@ -1,8 +1,28 @@
 import { useProducts } from "../../hooks/useProducts";
 import classes from "./ProductsCards.module.css";
+import deleteIcon from "../../assets/delete.png";
 
 export const ProductsCards = () => {
   const { products, loading, error } = useProducts();
+
+  const handleRemove = async (id: string) => {
+    const confirm = window.confirm("¿Seguro que desea eliminar el producto?");
+    if (!confirm) return;
+
+    const response = await fetch(
+      `https://mundo-gatuno-backend.onrender.com/api/products/${id}`,
+      {
+        method: "DELETE",
+      }
+    );
+
+    if (response.ok) {
+      alert("Producto eliminado con éxito");
+      location.reload();
+    } else {
+      alert("Error al eliminar el producto");
+    }
+  };
 
   if (loading) return <p>Cargando...</p>;
   if (error) return <p>{error}</p>;
@@ -10,19 +30,26 @@ export const ProductsCards = () => {
 
   return (
     <section className={classes["products-cards"]}>
-      <h1>Arena disponible</h1>
       <ul className={classes["products-cards__list"]}>
         {products.map((product) => (
           <li
             key={product._id}
             className={classes["products-cards__list--item"]}
           >
+            <div className={classes["products-cards__list--header"]}>
+              <h1>Arena disponible</h1>
+              <img
+                src={deleteIcon}
+                alt="Remove icon"
+                onClick={() => handleRemove(product._id)}
+              />
+            </div>
             <h2>{product.title}</h2>
             <p>{product.description}</p>
             <p>{product.price}</p>
             <p>{product.quantity}</p>
             <p>{product.aroma}</p>
-           <p>{product.weightKg} kg</p>
+            <p>{product.weightKg} kg</p>
             <img
               src={product.image}
               alt={product.title}
