2af38555d2f217d4065da85daa60e6d264219c18
devdiegomt <devdiegomt@gmail.com>
2026-07-17 20:12:31 -0500

    refactor: css updated and login feature added.

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,23 +1,39 @@
+import { useRef, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 import { ProductsCards } from "../../components/products/ProductsCards";
-import { useRef } from "react";
 import { ProductForm } from "../../components/products/ProductForm";
 import type { ProductFormRef } from "../../components/products/ProductForm";
+import type { Product } from "../../types/product";
 import classes from "./Products.module.css";
 
 export const Products = () => {
+  const products = useLoaderData() as Product.Props[];
   const formRef = useRef<ProductFormRef>(null);
+  const [productToEdit, setProductToEdit] = useState<Product.Props | undefined>(
+    undefined
+  );
+
+  const handleAdd = () => {
+    setProductToEdit(undefined);
+    formRef.current?.open();
+  };
+
+  const handleEdit = (product: Product.Props) => {
+    setProductToEdit(product);
+    formRef.current?.open();
+  };
 
   return (
     <>
       <button
         type="button"
-        onClick={() => formRef.current?.open()}
+        onClick={handleAdd}
         className={classes["add-button"]}
       >
         +
       </button>
-      <ProductForm ref={formRef} />
-      <ProductsCards />
+      <ProductForm ref={formRef} productToEdit={productToEdit} />
+      <ProductsCards products={products} onEdit={handleEdit} />
     </>
   );
 };
